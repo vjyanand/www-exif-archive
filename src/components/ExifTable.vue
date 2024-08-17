cf26e90266106bea3c954c691f5cc801b20e392a
Vjy <vijay@iavian.com>
2024-08-17 15:49:01 +0530

    nyp

---
+++ src/components/ExifTable.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="exif-div">
+
+  </div>
+</template>
+
+<style scoped>
+
+.flex-container {
+    display: flex;
+}
+
+table,
+th,
+td {
+    border: 1px solid rgb(207, 207, 207);
+    border-collapse: collapse;
+}
+
+th,
+td {
+    padding: 10px;
+}
+
+tr:nth-child(odd) {
+    background-color: #f9f9f9;
+}
+tr:nth-child(even) {
+    background-color: #fcfcfc;
+}
+
+</style>
