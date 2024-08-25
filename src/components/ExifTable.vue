31b3cf7a215dc21b01b53e5bdaca68c1bb54cb65
Vjy <vijay@iavian.com>
2024-08-25 17:43:57 +0530

    nyp

--- src/components/ExifTable.vue
+++ src/components/ExifTable.vue
@@ -67,6 +67,18 @@ onMounted(() => {
     <table class="select-none font-lato max-w-5xl mx-auto my-2" ref="mtable"></table>
     <NoTable v-if="noTableData" />
   </div>
+
+
+
+<!-- Light style tooltip -->
+
+<button data-tooltip-target="tooltip-light" data-tooltip-style="light" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Light tooltip</button>
+
+<div id="tooltip-light" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 tooltip">
+    Tooltip content
+    <div class="tooltip-arrow" data-popper-arrow></div>
+</div>
+
 </template>
 
 <style scoped>
