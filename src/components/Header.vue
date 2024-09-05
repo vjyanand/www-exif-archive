378760e8a25ebc5bac35cd6b107bd3cbe9039382
SnehaSoom <soomsneha05@gmail.com>
2024-09-05 14:56:54 +0530

    first page done

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -174,8 +174,7 @@ const closeMobileMenu = () => {
 
             <a
               class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 hover:text-blue-700"
-              href="mailto:help@exif.iavian.net"
-            >
+              href="mailto:help@exif.iavian.net"             >
               Contact
             </a>
           </div>
