7c8cc310993c6c84aa6add2b2fde25e5e2cb5782
SnehaSoom <soomsneha05@gmail.com>
2024-09-05 14:32:01 +0530

    first page done

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="bg-neutral-100 shadow dark:bg-gray-800">
     <div class="max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400 ml-[14rem]">© 2024 <a href="https://flowbite.com/"
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400 mr-[14rem]">© 2024 <a href="https://flowbite.com/"
           class="hover:underline">Vijay</a>. All Rights Reserved.
       </span>
       <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0 gap-4">
